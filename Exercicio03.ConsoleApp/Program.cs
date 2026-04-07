4a7d63f7f610261cc96a6327bc60936861fa4418
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-04-07 12:27:05 -0300

    Add cylinder volume calculation and update exercise list

--- Exercicio03.ConsoleApp/Program.cs
+++ Exercicio03.ConsoleApp/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace Exercicio03.ConsoleApp
 {
+    class Calculadora
+    {
+        private double pi = 3.14159; 
+        private double raio;
+        private double altura;
+
+        public Calculadora(double raio, double altura)
+        {
+            this.raio = raio;
+            this.altura = altura;
+        }
+
+        public double Volume()
+        {
+            return pi * Math.Pow(raio, 2) * altura;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Digite o raio: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Calculadora calcular = new Calculadora(raio, altura);
+            double volume = calcular.Volume();
+
+            Console.WriteLine($"O volume do cilindro é: {volume:F2}");
+            Console.ReadLine();
         }
     }
 }
